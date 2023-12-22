987a20f1f0aacae362f59cd9373485b09ab318ef
Neutral <147939975+neutral13@users.noreply.github.com>
2023-12-22 20:44:31 +0600

    Update weather.py

--- weather.py
+++ weather.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 
 
-def get_current_weather():
+def get_current_weather(city):
     request_url = f'https://api.openweathermap.org/data/2.5/weather?&appid={os.env["API_KEY"]}&q={city}&units=metric'
     weather_data = requests.get(request_url).json()
     return weather_data
